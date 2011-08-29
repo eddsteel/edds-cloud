f8baf4452968a12680cc1add7f8e0db388b5bd4e
Edd Steel <edward.steel@gmail.com>
2011-08-29 16:16:48 -0700

    add QR code

--- lib/edds-cloud.rb
+++ lib/edds-cloud.rb
@@ -45,6 +45,7 @@ end
 get '/cv' do redirect '/bucket/cv.pdf' end
 get '/resume' do redirect '/bucket/cv.pdf' end
 get '/about' do redirect '/' end
+get '/qr' do redirect '/about' end
 
 ##
 # List entries
